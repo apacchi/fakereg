e248c2eafb4e4bd8fd2b6c608addd9df67ff15b7
apacchi <apacchidesu@gmail.com>
2026-09-20 15:28:53 +0800

    Update dllmain.cpp

--- version/dllmain.cpp
+++ version/dllmain.cpp
@@ -1,5 +1,25 @@
-﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
+
+#pragma region Forward functions to system version.dll (via /EXPORT, not .def forwarder)
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoA=version_o.GetFileVersionInfoA")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoByHandle=version_o.GetFileVersionInfoByHandle")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoExA=version_o.GetFileVersionInfoExA")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoExW=version_o.GetFileVersionInfoExW")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeA=version_o.GetFileVersionInfoSizeA")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeExA=version_o.GetFileVersionInfoSizeExA")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeExW=version_o.GetFileVersionInfoSizeExW")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoSizeW=version_o.GetFileVersionInfoSizeW")
+#pragma comment(linker, "/EXPORT:GetFileVersionInfoW=version_o.GetFileVersionInfoW")
+#pragma comment(linker, "/EXPORT:VerFindFileA=version_o.VerFindFileA")
+#pragma comment(linker, "/EXPORT:VerFindFileW=version_o.VerFindFileW")
+#pragma comment(linker, "/EXPORT:VerInstallFileA=version_o.VerInstallFileA")
+#pragma comment(linker, "/EXPORT:VerInstallFileW=version_o.VerInstallFileW")
+#pragma comment(linker, "/EXPORT:VerLanguageNameA=version_o.VerLanguageNameA")
+#pragma comment(linker, "/EXPORT:VerLanguageNameW=version_o.VerLanguageNameW")
+#pragma comment(linker, "/EXPORT:VerQueryValueA=version_o.VerQueryValueA")
+#pragma comment(linker, "/EXPORT:VerQueryValueW=version_o.VerQueryValueW")
+#pragma endregion
 
 bool FileExists(LPCTSTR filename) {
 	HANDLE hFile = CreateFile(
